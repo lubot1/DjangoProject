f51e9939b5d6b636347046679b53528e40bd7ef6
Luis Botello <botello468@gmail.com>
2021-06-10 20:08:18 -0400

    Added items model to Django

--- Marketplace/models.py
+++ Marketplace/models.py
@@ -1,3 +1,12 @@
+import os
+from django.conf import settings
 from django.db import models
 
+def image_dir():
+    return os.path.join(settings.BASE_DIR, 'Images')
 # Create your models here.
+class Item(models.Model):
+    title = models.CharField(max_length=255)
+    file = models.FilePathField(path=image_dir)
+    description = models.TextField()
+    # lister = models.ForeignKey(Users,on_delete="cascade")
